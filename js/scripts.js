435752e1a19f8025ceb5d8783719d779289b3262
Magda Balun <magi@op.pl>
2017-11-24 14:43:30 +0100

    Add index.html and js folder and file

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+alert('Calculation of the result of operation: (a * a) - (2 * a * b) + (b * b)');
+
+var a = parseInt(prompt("Enter number a : ")),
+	b = parseInt(prompt("Enter number b : ")),
+	value = (a * a) - (2 * a * b) + (b * b);
+alert('The result of operation: (' + a + '*' + a + ') - (2 *' + a + '*' + b + ') + (' + b + '*' + b + ') is equal to: ' + value + '.');
+console.log('The result of operation: (' + a + '*' + a + ') - (2 *' + a + '*' + b + ') + (' + b + '*' + b + ') is equal to: ' + value + '.');
+
+if (value > 0) {
+	console.log('Positive result.');
+} else if (value < 0) {
+	console.log('Negative result.');
+} else {
+	console.log('The result is equal to 0.');
+}
+
+var value = value == 0 ? ('Value is equal to 0.') : ('Value is diffrent of 0.');
+console.log(value);
